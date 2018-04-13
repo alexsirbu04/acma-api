eaf948f2ed9546e1b6beac9e9ddcb1bb7036e7cc
alexsirbu04 <alex.sirbu@tmnet.ro>
2018-04-13 21:29:09 +0300

    Test commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send({ hello: 'world' });
+  response.send({ hi: 'there' });
 });
 
 const PORT = process.env.PORT || 5000;
