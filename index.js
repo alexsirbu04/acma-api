468198fe8362543a5e073fc2c70cfd940b8edc95
Alex <alex.sirbu@tmnet.ro>
2018-05-03 17:58:49 +0300

    Updated hotel data

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 require('./models/Hotel');
+
 require('./populate');
+
 const AppRouter = require('./router');
 const router = AppRouter.Router;
 
