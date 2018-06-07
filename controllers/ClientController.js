96a513b67f43fc8e8348a5552955b6d763ca138d
Alex <alex.sirbu@tmnet.ro>
2018-06-07 20:53:48 +0300

    Changed the logic for adding clients

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -3,9 +3,13 @@ const Client = require("../models/Client");
 exports.add = (req, res, next) => {
   const clients = req.body.clients;
   for (const client of clients) {
-    const doc = new Client(client);
-    doc.save(err => {
+    Client.findOne({ phone: client.phone }, (err, existingClient) => {
       if (err) return next(err);
+      if (existingClient) return next();
+      const doc = new Client(client);
+      doc.save(err => {
+        if (err) return next(err);
+      });
     });
   }
   res.json({ added: true });
