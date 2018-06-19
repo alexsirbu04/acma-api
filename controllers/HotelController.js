7a2fcc0db7a16e8e89b2bc0df93ceec1f8682853
alexsirbu04 <alex.sirbu@tmnet.ro>
2018-06-19 13:15:05 +0300

    Small refactor in hotelController

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -1,16 +1,10 @@
 const Hotel = require("../models/Hotel");
 
 exports.getHotels = (req, res, next) => {
-  // Find all hotels and return a JSON response
   Hotel.find()
     .lean()
     .exec((err, hotels) => {
       if (err) return next(err);
-      res.json({
-        // Iterate through each hotel
-        hotels: hotels.map(hotel => ({
-          ...hotel
-        }))
-      });
+      res.json({ hotels });
     });
 };
